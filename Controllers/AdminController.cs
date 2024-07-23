c16400bb483783c57c23c06974594ae11f3ced50
untitle <cdmikunj1347@gmail.com>
2024-07-23 13:51:02 +0530

    ecommerce upload

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_commerce.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult register()
+        {
+            return View();
+        }
+
+        public IActionResult login()
+        {
+            return View(); 
+        }
+    }
+}
